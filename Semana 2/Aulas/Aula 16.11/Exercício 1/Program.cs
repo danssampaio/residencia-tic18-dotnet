f79dc1ae6253dfa2ab97f3125fe1b8ae64238edc
Dan Souza <darleyble3@gmail.com>
2023-11-16 15:31:37 -0300

    weak 2

---
+++ Semana 2/Aulas/Aula 16.11/Exercício 1/Program.cs
@@ -0,0 +1,50 @@
+﻿#region 0 -> 30
+
+Console.WriteLine("\n --- Divisores --- \n");
+
+for (int i = 0; i <= 30; i++)
+{
+    if (i % 3 == 0 || i % 4 == 0)
+        Console.Write(i + " ");
+}
+
+Console.WriteLine();
+
+#endregion
+
+#region fibonacci
+
+Console.WriteLine("\n --- Fibonacci --- \n");
+
+int fib1 = 0, fib2 = 1, fib;
+Console.Write(fib1 + " " + fib2 + " ");
+fib = fib1 + fib2;
+while (fib <= 100)
+{
+    Console.Write(fib + " ");
+    fib1 = fib2;
+    fib2 = fib;
+    fib = fib1 + fib2;
+}
+
+Console.WriteLine();
+
+#endregion
+
+
+#region Tabela 
+
+Console.WriteLine("\n --- Tabela --- \n");
+
+int value = 8;
+
+for(int i = 1; i <= value; i++){
+    for(int j = 1; j <= i; j++){
+        Console.Write(i*j + " ");
+    }
+    Console.WriteLine();
+}
+
+#endregion
+
+Console.WriteLine();

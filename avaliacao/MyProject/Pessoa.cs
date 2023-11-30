f3bab49bc99b9039af4045c53ce108787dd18345
Dan Souza <darleyble3@gmail.com>
2023-11-30 16:29:40 -0300

    update pessoa and create main for test

--- avaliacao/MyProject/Pessoa.cs
+++ avaliacao/MyProject/Pessoa.cs
@@ -3,28 +3,12 @@ public class Pessoa
 {
     public string Nome { get; set; }
     public DateTime DataNascimento { get; set; }
-    private string CPF;
-    public string cpf
-    {
-        get { return CPF; }
-        set
-        {
-            if (CpfValido(value))
-                CPF = value;
-            else
-                throw new CpfIsNotValidException();
-        }
-    }
+    public string CPF { get; set; }
 
     public Pessoa(string nome, DateTime dataNascimento, string cpf)
     {
         Nome = nome;
         DataNascimento = dataNascimento;
         CPF = cpf;
-    }
-
-    private bool CpfValido(string cpf)
-    {
-        return !string.IsNullOrEmpty(cpf) && cpf.Length == 11 && cpf.All(char.IsDigit);
     }
 }
--- avaliacao/MyProject/Program.cs
+++ avaliacao/MyProject/Program.cs
@@ -1,28 +1,104 @@
-﻿public class Program{
-    
+﻿namespace Namespace;
+class Program
+{
+    static Escritorio escritorio = new Escritorio();
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("---- Menu---- \n" +
+            "1. Adicionar Advogado\n" +
+            "2. Adicionar Cliente\n" +
+            "3. Sair\n");
+
+            Console.Write("Escolha uma opção: ");
+            string opcao = Console.ReadLine() ?? "";
+
+            Console.WriteLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    AdicionarAdvogado();
+                    Console.WriteLine();
+                    break;
+                case "2":
+                    AdicionarCliente();
+                    Console.WriteLine();
+                    break;
+                case "3":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+
+    static void AdicionarAdvogado()
+    {
+        Console.Write("Nome: ");
+        string nome = Console.ReadLine() ?? "";
+
+        Console.Write("Data de Nascimento (DD/MM/AAAA): ");
+        DateTime dataNascimento = DateTime.ParseExact(Console.ReadLine() ?? "", "dd/MM/yyyy", null);
+
+        Console.Write("CPF: ");
+        string cpf = Console.ReadLine() ?? "";
+
+        Console.Write("CNA: ");
+        string cna = Console.ReadLine() ?? "";
+
+        Advogado advogado = new Advogado(nome, dataNascimento, cpf, cna);
+        escritorio.AdicionarAdvogado(advogado);
+    }
+
+    static void AdicionarCliente()
+    {
+        Console.Write("Nome: ");
+        string nome = Console.ReadLine() ?? "";
+
+        Console.Write("Data de Nascimento (DD/MM/AAAA): ");
+        DateTime dataNascimento = DateTime.ParseExact(Console.ReadLine() ?? "", "dd/MM/yyyy", null);
+
+        Console.Write("CPF: ");
+        string cpf = Console.ReadLine() ?? "";
+
+        Console.Write("Estado Civil: ");
+        string estadoCivil = Console.ReadLine() ?? "";
+
+        Console.Write("Profissão: ");
+        string profissao = Console.ReadLine() ?? "";
+
+        Cliente cliente = new Cliente(nome, dataNascimento, cpf, estadoCivil, profissao);
+        escritorio.AdicionarCliente(cliente);
+    }
 }
+
 
 public class CpfNotFoundException : Exception
 {
-    public CpfNotFoundException() : base("CPF não encontrado."){}
+    public CpfNotFoundException() : base("CPF não encontrado.") { }
 }
 
 public class CpfIsNotValidException : Exception
 {
-    public CpfIsNotValidException() : base("CPF Inválido."){}
+    public CpfIsNotValidException() : base("CPF Inválido.") { }
 }
 
 public class CnaNotFoundException : Exception
 {
-    public CnaNotFoundException() : base("CNA não encontrado."){}
+    public CnaNotFoundException() : base("CNA não encontrado.") { }
 }
 
 public class RepeatedRegisterAttorneyException : Exception
 {
-    public RepeatedRegisterAttorneyException() : base("CPF ou CNA já cadastrado."){}
+    public RepeatedRegisterAttorneyException() : base("CPF ou CNA já cadastrado.") { }
 }
 
 public class RepeatedRegisterClientException : Exception
 {
-    public RepeatedRegisterClientException() : base("CPF já cadastrado."){}
+    public RepeatedRegisterClientException() : base("CPF já cadastrado.") { }
 }
